0e6073489b123a86ee0ec34eae6742281f329902
Zhaeong <owen_zzz@hotmail.com>
2021-08-14 21:25:13 -0400

    formatting

--- PrimeTest.cpp
+++ PrimeTest.cpp
@@ -10,9 +10,10 @@ bool isPrime(int n)
     // middle five numbers in below loop 
     if (n%2 == 0 || n%3 == 0) return false; 
   
-    for (int i=5; i*i<=n; i=i+6) 
+    for (int i=5; i*i<=n; i=i+6){ 
         if (n%i == 0 || n%(i+2) == 0) 
            return false; 
+    }
   
     return true; 
 } 
